45133c9c47d726758abe0a8dcb169d5a5bd6ee66
lewisseo91 <togokr@naver.com>
2020-04-22 02:09:49 +0900

    add virus info

--- src/components/articles/articles.js
+++ src/components/articles/articles.js
@@ -4,11 +4,7 @@ import './articles.scss';
 class Articles extends Component {
     render () {
         const articles_data = [
-            { link : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/', title : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/' },
-            { link : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/', title : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/' },
-            { link : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/', title : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/' },
-            { link : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/', title : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/' },
-            { link : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/', title : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/' },
+            { link : 'https://scopeblog.stanford.edu/2020/04/02/whats-a-virus-anyway-part-1-the-bare-bones-basics/', title : 'https://scopeblog.stanford.edu/2020/04/02/whats-a-virus-anyway-part-1-the-bare-bones-basics/' },
             { link : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/', title : 'https://scopeblog.stanford.edu/2020/04/03/how-coronaviruses-infect-us-how-infectious-viruses-created-us/' },
         ];
         const articles = articles_data.map(({id, link, title}, index) => (
